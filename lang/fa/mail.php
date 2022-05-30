8eb8c5aa39580dcc47911d33a366bcccbb03dc20
Mj Pakzad <mj.pakzad@gmail.com>
2022-05-31 02:44:07 +0430

    Add fa localization files to lang directory
    Add fa.json file and fa directory to the lang direcory.

---
+++ lang/fa/mail.php
@@ -0,0 +1,16 @@
+
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Mail Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the mail.
+    |
+    */
+    'regards'   => 'با احترام',
+    'name'      => 'باورژن',
+    'hello'     => 'سلام',
+];
